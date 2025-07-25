5ccc0eac708f293c632b07581eae7ea8d8be90d0
Claudio Matheus Dos Santos <clados-s@c2r10p3.42sp.org.br>
2025-07-25 17:06:24 -0300

    desgraça de split

--- ft_split.c
+++ ft_split.c
@@ -10,50 +10,83 @@
 // /*                                                                            */
 // /* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
-// char	**ft_split(char const *s, char c)
-// {
-// 	int				i;
-// 	int				w_count;
-// 	unsigned char	**new_str;
+char	**ft_split(char const *s, char c)
+{
+	size_t			i;
+	size_t			j;
+	int				start;
+	int				w_count;
+	unsigned char	**new_str;
 	
+	i = 0;
+	j = 0;
+	start = -1;
+	if (!s[i])
+		return (NULL);
+	w_count = count_words(s, c);
+	new_str = (char **)malloc(( w_count + 1) * sizeof(char *));
+	if (!new_str)
+		return (NULL);
 	
-// 	i = -1;
-// 	if (!s[i])
-// 		return (NULL);
-// 	new_str = malloc((unsigned int)count_words(s, c) + 1 * sizeof(char **));
-// 	if (!new_str)
-// 		return (NULL);
-// 	while (s[i])
-// 	{
-// 		while (s[++i] && count_sub(s, c, i))
-// 			new_str = malloc((unsigned int)count_sub(s, c, i) + 1 * sizeof(char **));
-			
-		
-// 	}
-	
-	
-// }
+}
 
-// static	size_t count_words(const char *s, char c)
-// {
-// 	int	i;
-// 	int	word;
 
-// 	i = 0;
-// 	word = 0;
-// 	while (s[i])
-// 	{
-// 		while (s[i] == c && s)
-// 			i++;
-// 		if (s[i])
-// 			word++;
-// 		while (s[i] != c && s[i])
-// 			i++;
-// 	}
-// 	return (word);
-// }
+static int	**fill_array(char const *s, char c, char *new_str)
+{
+	size_t	j;
+	size_t	i;
+
+	j = 0;
+	i = 0;
+	while (i < ft_strlen(s))
+	{
+		if (s[*i] != c && start < 0)
+			*start = i;
+		else if (s[*i] == c || s[*i] == '\0' && start >=0)
+		{
+			new_str[*j] = ft_substr(s, start, i - start);
+			if (!new_str[*j])
+				return (ft_free_split(new_str));
+			j++;
+			start = -1;
+		}
+		i++;
+	}
+	
+}
+
+static size_t	count_words(const char *s, char c)
+{
+	int	i;
+	int	word;
+
+	i = 0;
+	word = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i])
+			word++;
+		while (s[i] != c && s[i])
+			i++;
+	}
+	return (word);
+}
+
+static void	ft_free_split(char **str_array)
+{
+	int	i;
+
+	i = -1;
+	while (str_array[++i])
+		free(str_array[i]);
+	free(str_array);
+}
+
+
 
 // static	size_t count_sub(const char *s, char c, char *i)
 // {
@@ -78,7 +111,7 @@
 // 	return (sub);
 // }
 
-// int main(void)
-// {
-// 	printf("%d", count_words("xxxxhelloxxxxxhelloxxhelloxxhello", "x"));
-// }
+int main(void)
+{
+	printf("%d", count_words("xxxxhelloxxxxxhelloxxhelloxxhello", "x"));
+}
