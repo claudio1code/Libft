8c346eae1b7a38d09b8f13564e9ab30cb1508150
Claudio Matheus <claudio1.code@gmail.com>
2025-10-13 17:05:07 -0300

    feat: implementa duas ft

--- srcs/memory/free_split.c
+++ srcs/memory/free_split.c
@@ -6,7 +6,7 @@
 /*   By: clados-s <clados-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 16:14:01 by clados-s          #+#    #+#             */
-/*   Updated: 2025/10/09 16:28:34 by clados-s         ###   ########.fr       */
+/*   Updated: 2025/10/13 17:03:53 by clados-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	free_split(char **str_array)
 	int	i;
 
 	i = -1;
+	if (!str_array)
+		return ;
 	while (str_array[++i])
 		free(str_array[i]);
 	free(str_array);
